8cf3bb7928e98a72d006f97f44477272c36dac67
abdollahi <azitaabdollahi75@gmail.com>
2023-04-09 15:49:29 +0330

    feat: create sequelize model.

---
+++ src/models/note.model.ts
@@ -0,0 +1,38 @@
+import { sequelize, DataTypes} from "../utils/db";
+
+const NoteModel = sequelize.define("notes", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING(50),
+        allowNull: true
+    },
+    published: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull: false
+    }
+});
+export default NoteModel;
